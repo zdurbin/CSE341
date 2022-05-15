aaf005cdc2586ef763c30fc7b54c66910370b159
zdurbin <dur15014@byui.edu>
2022-05-14 22:11:10 -0700

    updates

--- swagger.js
+++ swagger.js
@@ -15,7 +15,4 @@ const endpointsFiles = ['./routes/index.js'];
 // generate swagger.json
 swaggerAutogen(outputFile, endpointsFiles, doc);
 
-// Run server after it gets generated
-// swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
-//   await import('./index.js');
-// });
+
