13c0667d1652703ad5d6f008d85e9a9b4b69588a
zdurbin <dur15014@byui.edu>
2022-05-14 18:50:00 -0700

    catch up

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
-const routes = require('express').Router();
+const express = require('express');
+const router = express.Router();
 
-routes.get('/', (req, res) => {
-  res.send('Hello World');
-});
+router.use('/contacts', require('./contacts'));
 
-module.exports = routes;
+module.exports = router;
