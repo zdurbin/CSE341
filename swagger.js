1e893fe789349e9a72b3deb7f8fafb9d28d201e2
zdurbin <dur15014@byui.edu>
2022-05-14 21:48:50 -0700

    update week 4

---
+++ swagger.js
@@ -0,0 +1,21 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'My API',
+    description: 'My contact API'
+  },
+  host: 'https://cse341node1.herokuapp.com/',
+  schemes: ['https']
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+
+// generate swagger.json
+swaggerAutogen(outputFile, endpointsFiles, doc);
+
+// Run server after it gets generated
+// swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
+//   await import('./index.js');
+// });
